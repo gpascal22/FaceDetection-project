a1a554980e828976ae9d73f9345fac93603deb87
Pascal Gautam <gautampascal@gmail.com>
2020-11-29 02:06:54 -0600

    added cascade_classify function

---
+++ files/cascade_classify.m
@@ -0,0 +1,17 @@
+function result = cascade_classify(window)
+% Applying a Cascade to a Window
+
+threshold = 1;
+k = size(window);
+
+for i = 1: k
+    if window < thresohold
+        window = -1;
+        result = window;
+    elseif window > threshold
+        window = 1;
+        result = window;
+    end
+end
+
+end
