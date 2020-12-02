e92c02c8a4ab051ffc1c7e92c007586179cc6217
Pascal Gautam <gautampascal@gmail.com>
2020-12-01 21:40:02 -0600

    updated test

--- test.m
+++ test.m
@@ -1,13 +1,14 @@
 % Advanced face detection with AdaBoost
 % Components: AdaBoost, Skin detection, Bootstrapping, Cascades.
 
-% TESTING script 
+% TESTING script
 
-%%
-
-clear all;
+clear;
 clc;
 
+addpath 'C:\Users\gauta\Desktop\ComputerVison-project-master\data\test_cropped_faces';
+addpath 'C:\Users\gauta\Desktop\ComputerVison-project-master\data\test_nonfaces';
+addpath 'C:\Users\gauta\Desktop\ComputerVison-project-master\data\test_face_photos';
 % set directories
 directories;
 
@@ -18,119 +19,123 @@ load boosted15;
 load training_data;
 
 threshold = 5;
-
 %%
-% Testing without classifier cascades and skin detection
-
+%Testing without classifier cascades and skin detection
 % 1. Testing cropped faces 
 
 testing_cropped_faces_path = [training_directory,'\', 'test_cropped_faces'];
 testing_cropped_faces_list = dir(testing_cropped_faces_path);
+testing_cropped_faces_list = remove_unwanted_dir(testing_cropped_faces_list);
 
 num_test_croppedFaces = size(testing_cropped_faces_list, 1);
 
 missclassified = 0;
 predRight1 = 0;
-dirData_croppedFace = dir('training_test_data\test_cropped_faces\*.bmp'); 
 
-for i = 1: length(dirData_croppedFace)
-    filename = dirData_croppedFace(k).name;
-    image1 = read_gray(fullfile('training_test_data\test_cropped_faces', filename));
-    data2 = imresize(image1,[50 50]);     
-    imwrite(data2, fullfile('ResizedTraining', filename));
-end
-
-corppedFaces = zeros(50, 50, 3046);
-
-dirResizedTranining = dir('ResizedTraining\*.bmp');
-numTestCroppedFaces = length(dirResizedTranining);
-for i = 1:numTestCroppedFaces
-    filename = numTestCroppedFaces(i).name;
-    imageA = imread(fullfile('ResizedTraining', filename));
-    corppedFaces(:,:,i) = reshape(imageA, [50, 50, 1]);
+for i =1:num_test_croppedFaces
+    cropped_face = testing_cropped_faces_list(i).name;
+    cropped_pic = double(imread(cropped_face));
+    center = (size(cropped_pic)/2)/2;
+    cropped_pic = imcrop(cropped_pic, [center 59 59]);
+   
+    result = apply_classifier_aux(cropped_pic, boosted_classifier, weak_classifiers, [60 60]);
+    class = result(31,31);
     
-    result = apply_classifier_aux(croppedFaces, boosted_classifier, weak_classifiers, [50 50]);
-    class = result(30, 30);
-    
-    if class > threshold
-        predRight1 = predRight1 + 1;
+    if class > threshold 
+        predRight1 = predRight1 +1;
     else
-        missclassified = missclassified + 1;
+        missclassified = missclassified +1;
     end
-    
+  
 end
 
-croppedFaceAccuracy = (predRight1 / numTestCroppedFaces) * 100;
+croppedFace_accuracy = (predRight1/num_test_croppedFaces) * 100;
+disp(croppedFace_accuracy);
 %%
-% 1. Testing cropped faces 
+% 1. Testing non faces 
+testing_nonfaces_path = [training_directory, '\', 'test_nonfaces'];
+testing_nonfaces_list = dir(testing_nonfaces_path);
+testing_nonfaces_list = remove_unwanted_dir(testing_nonfaces_list);
+num_testing_nonfaces = size(testing_nonfaces_list, 1);
 
-testing_cropped_faces_path = [training_directory, 'test_cropped_faces'];
-testing_cropped_faces_list = dir(testing_cropped_faces_path);
-
-num_test_croppedFaces = size(testing_cropped_faces_list, 1);
-
-missclassified = 0;
 predRight2 = 0;
-dirData_nonFaces = dir('training_test_data\test_nonfaces\*.bmp'); 
+missclassified = 0;
 
-for i = 1: length(dirData_nonFaces)
-    filename = dirData_nonFaces(k).name;
-    image1 = read_gray(fullfile('training_test_data\test_nonfaces', filename));
-    result = boosted_multiscale_search(image1, 3, boosted_classifier, weak_classifier, [50, 50], 1);
-    classN = max(max(result));
+for i =1:num_testing_nonfaces
+  
+    non_face = testing_nonfaces_list(i).name;
+    non_facePic = double(imread(non_faces));
     
-    if classN <= threshold
-        predRight2 = predicted + 1;
+    result = apply_classifier_aux(non_facePic, boosted_classifier, weak_classifiers, [60 60]);
+    %result = boosted_multiscale_search(non_facePic, 3, boosted_classifier, weak_classifiers, [60,60]);
+    
+    class = max(max(result));                    
+                          
+    if class <= threshold
+        predRight2 = predRight2 +1;
     else
-        missclassified = missclassified + 1;
+        missclassified = missclassified +1;
     end
+   
 end
-
-nonFacesAccuracy = (predRight2 / length(dirData_nonFaces)) * 100;
+nonFace_accuracy = (predRight2/num_testing_nonfaces) * 100;
+disp(nonFace_accuracy);
 
 %%
 % Testing photos
+testing_faces_path = [training_directory,'\', 'test_face_photos'];
+testing_faces_list = dir(testing_faces_path);
+testing_faces_list = remove_unwanted_dir(testing_faces_list);
 
-missclassified = 0;
+
+num_testing_faces = size(testing_faces_list, 1);
+
 predRight3 = 0;
-dirData_face_photos = dir('training_test_data\test_face_photos\*.bmp'); 
-num_face_photos = length(dirData_face_photos);
-for i = 1: num_face_photos
-    filename = num_face_photos(k).name;
-    imagePhoto = read_gray(fullfile('training_test_data\test_face_photos', filename));
-    result = boosted_multiscale_search(imagePhoto, 2, boosted_classifier, weak_classifier, [50, 50], 1);
-    classP = max(max(result));
+missclassified = 0;
+total = 0;
+
+for i =1:num_testing_faces
     
-    if classP > threshold
-        predRight3 = predicted + 1;
+    test_photo = testing_faces_list(i).name;
+    read_pic = read_gray(test_photo);
+    result = boosted_multiscale_search(read_pic, 2, ...
+                              boosted_classifier, weak_classifiers, ...
+                              [60,60]);
+                    
+    class = max(max(result));
+    
+    if class > threshold
+        predRight3 = predRight3 + 1;
     else
         missclassified = missclassified + 1;
     end
+  
 end
 
-facePhotoAccuracy = (predRight3 / num_face_photos) * 100;
-
+FaceAcc = (predRight3/num_testing_faces) * 100;
+disp(FaceAcc);
 %%
-% Testing with skin detection
-
+%Testing with skin detection
 % read histogram
 negative_histogram = read_double_image('negatives.bin');
 positive_histogram = read_double_image('positives.bin');
 
-for i = 1: num_face_photos
-    filename = num_face_photos(k).name;
-    imagePhoto = double(imread((fullfile('training_test_data\test_face_photos', filename)));
-    imshow(imagePhoto/255);
-    imagePhoto2 = read_gray(imagePhoto);
+for i =1:num_testing_faces
+    first_image = testing_faces_list(i).name;
+    sec_image = double(imread(first_image));
+    pass_image = read_gray(first_image);
     
-    if(size(imagePhoto, 3) == 3)
-        resultSkin = detect_skin(imagePhoto, positive_histogram, negative_histogram);
-        figure(i); imshow(resultSkin, []);
-        result = boosted_detector_demo(imagePhoto2, imagePhoto2, 2, boosted_classifier, weak_classifiers, [50, 50], 4);
+    if(size(sec_image, 3) == 3)
+        result_on_skin = detect_skin(sec_image, positive_histogram,  negative_histogram);
+        figure (i); imshow(result_on_skin, []);
+        
+        result = boosted_detector_demo(pass_image, 2,  boosted_classifier, ...
+                          weak_classifiers, [60,60], 4);
         figure(i); imshow(result, []);
     end
+      
 end
-    
+
 %%
 cascade_faces = [];
 cascade_nonfaces = [];
@@ -146,7 +151,7 @@ cascade_nonfaces = [];
  %      cascade_nonfaces = result;
  %  end
     
-end
+%end
 
 
     
